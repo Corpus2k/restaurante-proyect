c702847b4f2ded2b43facd8e945f11be24e5e710
Alvaro Palomino <helixsy06@gmail.com>
2025-04-09 11:35:57 -0500

    Funcion de Boleta añadida, genera un archivo PDF autodescargable

--- src/app/category-interface/category-interface.component.ts
+++ src/app/category-interface/category-interface.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { CATEGORIES } from '../data/products';
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
 
 @Component({
   selector: 'app-category-interface',
@@ -86,5 +88,51 @@ export class CategoryInterfaceComponent {
 
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
+  }
+  //ESTA ES LA FUNCION PARA GENERAR LA BOLETA,NO LA TOQUEN HDPS
+  generarBoleta(){
+    const doc = new jsPDF();
+
+    doc.setFontSize(18);
+    doc.text('SteackHouse Restaurant', 105, 20, {align:'center'});
+
+    doc.setFontSize(12);
+    doc.text(`Fecha: ${new Date().toLocaleString()}`, 14, 30);
+    doc.text(`Usuario: ${this.username || 'No Definido'}`, 14, 38);
+    doc.text(`Mesa: ${this.seatCount}`, 14, 45);
+    doc.text(`Asientos ${this.selectedSeats.join(', ') || 'Ninguno'}`, 14, 52);
+
+
+    doc.setDrawColor(150);
+    doc.line(14, 58, 196, 58);
+
+
+    const items = this.cart.map((item)=>[
+      item.nombre,
+      `S/ ${item.precio.toFixed(2)}`,
+      item.cantidad,
+      `S/ ${(item.precio * item.cantidad).toFixed(2)}`,
+    ]);
+
+    autoTable(doc,{
+      startY: 65,
+      head:[['Producto', 'Precio', 'Cant.', 'Subtotal']],
+      body: items,
+      styles:{
+        fontSize: 10,
+        cellPadding: 4,
+      },
+      headStyles:{
+        fillColor:[41, 128, 185],
+        textColor:[255, 255, 255],
+      },
+    });
+
+    const finalY = (doc as any).lastAutoTable.finalY || 75;
+    doc.setFontSize(14);
+    doc.setTextColor(0, 0, 0);
+    doc.text(`Total: S/ ${this.total.toFixed(2)}`, 150, finalY + 10);
+
+    doc.save('Boleta.pdf');
   }
 }
